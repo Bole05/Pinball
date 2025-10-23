8fa8c74f5d5983574864162a1eb9abde0bf541b9
yangfeng-xu <yangfeng.xu@estudiantat.upc.edu>
2025-10-23 14:13:26 +0200

    GF

--- Source/ModulePhysics.h
+++ Source/ModulePhysics.h
@@ -8,7 +8,7 @@
 #define GRAVITY_X 0.0f
 #define GRAVITY_Y -7.0f // <-- He cambiado este valor
 
-#define PIXELS_PER_METER 50.0f // if touched change METER_PER_PIXEL too
+#define PIXELS_PER_METER 50.0f // if touched change METER_PER_PIXEL tooJYT
 #define METER_PER_PIXEL 0.02f // this is 1 / PIXELS_PER_METER !
 
 #define METERS_TO_PIXELS(m) ((int) floor(PIXELS_PER_METER * m))
